151bf066b55ea589ebd193beba3b02baecba361e
Paulo Henrique <desenvolvedores@sauderesidence.com.br>
2019-01-09 19:03:46 -0300

    Criando botão

--- App.js
+++ App.js
@@ -1,55 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text} from 'react-native';
+import { View, Button} from 'react-native';
 
-const estilos = {
-
+const Estilos = {
   principal: {
-    flex: 1,
-    backgroundColor: 'cornflowerblue',
-    height: 500,
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    alignItems: 'stretch'
-  },
-
-  topo: {
-    flex: 1,
-    fontSize: 30,
-    backgroundColor: 'brown',
-    color: '#FFF',
-    textAlign: 'center',
-    
-  },
-
-  conteudo: {
-    flex: 5,
-    fontSize: 30,
-    backgroundColor: 'yellowgreen',
-    color: '#FFF',
-    textAlign: 'center',
-    
-  },
-
-  rodape: {
-    flex: 1,
-    fontSize: 30,
-    backgroundColor: 'orangered',
-    color: '#FFF',
-    textAlign: 'center',
-    
+    paddingTop: 40
   }
 };
+
+const botaoPressionado = () => { alert('Botão pressionado!') }
 
 export default class App2 extends Component {
   render() {
 
-    const { principal, topo, conteudo, rodape } = estilos;
+    const { principal } = Estilos;
 
     return (
       <View style={ principal }>
-          <Text style={ topo }>Topo</Text>
-          <Text style={ conteudo }>Conteudo</Text>
-          <Text style={ rodape }>Rodape</Text>
+        
+        <Button 
+          title="Clique aqui"
+          color="#841584"
+          onPress={botaoPressionado}
+          accessibilityLabel="Clique para ver as noticias"
+
+        />
+
       </View>
     )
   }
